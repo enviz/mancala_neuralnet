2fabaee432870264cae469ecc59b5b31bced002d
enviz <48526315+enviz@users.noreply.github.com>
2019-08-15 23:52:02 +0530

    Plots the accuracy and loss of both train and test
    Plots the accuracy and loss of both train and test

--- train.py
+++ train.py
@@ -21,19 +21,31 @@ history = model.fit(ips,ops,batch_size=sample_size,epochs=epochs,validation_spli
 
 model.save('new_model')
 fig = plt.figure()
-p1 = fig.add_subplot(121)
-p2 = fig.add_subplot(122)
+p1 = fig.add_subplot(221)
+p2 = fig.add_subplot(222)
+p3 = fig.add_subplot(223)
+p4 = fig.add_subplot(224)
 p2.set_ylim(0,1)
-p2.grid()
+p4.set_ylim(0,1)
 p1.grid()
+p2.grid()
+p3.grid()
+p4.grid()
 p2.set_yticks(np.arange(0,1,0.1))
+p4.set_yticks(np.arange(0,1,0.1))
 x = [i for i in range(epochs)]
 y = history.history['loss']
 y2 = history.history['acc']
-p1.plot(x,y, 'r', label='loss')
+y3 = history.history['val_loss']
+y4 = history.history['val_acc']
+p1.plot(x,y, 'r', label='train_loss')
 p1.legend()
-p2.plot(x,y2, 'b', label='accuracy')
+p2.plot(x,y2, 'b', label='train_accuracy')
 p2.legend()
+p3.plot(x,y3, 'r', label='val_loss')
+p3.legend()
+p4.plot(x,y4, 'b', label='val_accuracy')
+p4.legend()
 plt.show()
 
 
